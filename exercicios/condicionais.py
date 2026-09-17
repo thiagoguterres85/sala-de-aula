8b34b86e19c17c25d922dde81a229174d38dc0b3
THIAGO GUTERRES DE SOUSA <02260728006@senacrs.edu.br>
2026-09-16 21:58:22 -0300

    exercicios condicionais

---
+++ exercicios/condicionais.py
@@ -0,0 +1,29 @@
+def fizz_buzz (numero:int):
+    elif numero %3==0 and %5==0:
+    return "FizzBuzz"
+   
+    if numero %5==0:
+          return "Fizz"
+    
+    if numero  %3==0:
+          return "Buzz"
+    
+print (fizz_buzz)
+
+
+# Exercicio 2
+def verificar_maioridade(idade:int):
+    if idade >18:  
+        return "Maior de Idade" 
+
+    else:
+        return "Menor de Idade"
+    print (verificar_maioridade)
+
+        
+
+
+
+  
+
+    
