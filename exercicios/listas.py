c3a6281de86335f5b5ea888f5980fa9ba3b1cc1e
THIAGO GUTERRES DE SOUSA <02260728006@senacrs.edu.br>
2026-09-21 21:53:12 -0300

    exercicios listas

--- exercicios/listas.py
+++ exercicios/listas.py
@@ -11,5 +11,27 @@ def filtrar_pares (numeros:list):
 if __name__ == '__main__':
         numeros_pares = filtrar_pares([1, 2, 3, 4, 5, 6])
         
+
         print (numeros_pares)
+
+
+
+#Exercicio 2 
+def contar_negativos (numeros: list):
+    count = 0
+    for numero in numeros:
+         if numero < 0:
+             count+=1
+     
+    return "negativos"
+
+if __name__ == '__main__':
+    count = contar_negativos ([10, -3, 0, -5, 8, -1])
+    
+    print (count)    
+
+
+
+ 
+         
 
